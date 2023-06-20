d0d60171d67468331f270fa0128227abb08550a0
Girbau <lgirbau36@gmail.com>
2023-06-20 10:14:18 +0200

    express quête 4 correction

--- userList.js
+++ userList.js
@@ -30,9 +30,61 @@ const getUserById = (req, res) => {
     });
 };
 
+const postUser = (req,res) => {
+  const { firstname, lastname, email, city, langage} = req.body;
+
+  database
+  .query (
+    "INSERT INTO user(firstname, lastname, email, city, langage) VALUES (?,?,?,?,?)",
+    [firstname, lastname, email, city, language]
+  )
+  .then(([result]) => {
+    res.location(`/api/user/${result.insertId}`).sendStatus(201);
+  })
+ .catch((err) => {
+  console.error(err);
+  res.status(500).send("Error saving user")
+ });
+};
+
+constupdateUser = (req,res) => {
+  const id = parseInt(req.params.id);
+  const {firstname, lastname, email, city, langage} = req.body;
+  
+  database
+  .query (
+    "UPDATE users set firstname=?, email=?, city=?, langage=? where id = ?",
+    [firstname,lastname,email,city,langage, id]
+    )
+  .then(([result]) => {
+    if (result.affectedRows ===0) {
+      res.status(404).send("Not Found");
+    } else {
+      res.status(204);
+    }
+  })
+  .catch((error) => {
+    console.error(err);
+    res.status(500).send("Error editing the user");
+  });
+};
+
 module.exports = {
   getUsers,
   getUserById,
   postUser,
   updateUser,
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
